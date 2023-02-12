3e48393a68add31510e4e856d6b8239a91a863dc
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-02-11 21:32:32 -0800

    City

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ creating class city that inherits from BaseModel"""
+
+    state_id = ""
+    name = ""
