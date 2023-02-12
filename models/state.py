791cfb0bb999c5e75ca8545ea622e7af9ea82a07
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-02-11 21:29:13 -0800

    State

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ creating a new class named state that inherits
+    from BaseModel"""
+
+    name = ""
